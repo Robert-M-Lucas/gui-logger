9749f26e99c0a6d14197278d010eff64ab433915
robert-m-lucas <noreply@example.com>
2023-08-29 12:18:01 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-use std::thread::sleep;
-use std::time::Duration;
-
 pub fn gui_log_text(path: String, value: String) {
     let client = reqwest::blocking::Client::new();
     let result = client.put("http://localhost:8000/send")
@@ -13,14 +10,5 @@ pub fn gui_log_text(path: String, value: String) {
 
     if let Err(e) = result {
         println!("Log failed with error: {}", e);
-    }
-}
-
-fn main() {
-    let mut counter = 0;
-    loop {
-        gui_log_text("Label/From/Code/LabelName".to_string(), format!("{}", counter));
-        counter += 1;
-        sleep(Duration::from_millis(50));
     }
 }
